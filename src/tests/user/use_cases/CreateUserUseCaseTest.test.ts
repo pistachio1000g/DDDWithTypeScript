d398191f3da090ac68a65d87458b479986b73f86
Pistachio1000g <qwerty100a@gmail.com>
2021-08-14 00:08:10 +0900

    テストを実装

---
+++ src/tests/user/use_cases/CreateUserUseCaseTest.test.ts
@@ -0,0 +1,17 @@
+import { UserFactory } from '../../..//packages/factory/user/UserFactory'
+import { userRepository } from '../../../packages/infrastructure/user/UserRepository'
+import {
+  CreateUserCommand,
+  CreateUserUseCase,
+} from '../../../packages/user/use_cases/CreateUserUseCase'
+
+describe('CreateUserUseCaseTest', (): void => {
+  test('ユーザーが正しく作られるか.', (): void => {
+    const useCase = new CreateUserUseCase(
+      new UserFactory(),
+      new userRepository()
+    )
+
+    useCase.handler(new CreateUserCommand('名前', '名字', 20, 'FEMALE'))
+  })
+})
