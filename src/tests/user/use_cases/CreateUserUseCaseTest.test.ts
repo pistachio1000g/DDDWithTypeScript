af61d6144bf0461daeacb0bc475e8293e7870b4e
Pistachio1000g <qwerty100a@gmail.com>
2021-08-27 23:49:05 +0900

    uuidをモックできるように修正

--- src/tests/user/use_cases/CreateUserUseCaseTest.test.ts
+++ src/tests/user/use_cases/CreateUserUseCaseTest.test.ts
@@ -7,7 +7,15 @@ import {
 
 import 'jest-localstorage-mock'
 
+import uuid from 'uuid'
+
 describe('CreateUserUseCaseTest', (): void => {
+  jest.mock('uuid', () => ({
+    v4: () => {
+      return 'mockResponse'
+    },
+  }))
+
   test('ユーザーが正しく作られるか.', (): void => {
     const useCase = new CreateUserUseCase(
       new UserFactory(),
